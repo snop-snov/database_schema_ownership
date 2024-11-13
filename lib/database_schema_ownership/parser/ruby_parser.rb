1ccf53d21bee075da37caa0dca42f7685bdbea03
djezzzl <lawliet.djez@gmail.com>
2024-11-13 18:45:00 +0700

    update support of FKs to RubyParser

--- lib/database_schema_ownership/parser/ruby_parser.rb
+++ lib/database_schema_ownership/parser/ruby_parser.rb
@@ -12,11 +12,8 @@ module DatabaseSchemaOwnership
       end
 
       def parse_foreign_keys
-        schema.scan(/\n*(\s+add_foreign_key "([^"]*)", "([^"]*)")/).flat_map do |s|
-          [
-            DatabaseSchemaOwnership::Entity.new(s[1], s[0]),
-            DatabaseSchemaOwnership::Entity.new(s[2], s[0])
-          ]
+        schema.scan(/\n*(\s+add_foreign_key "([^"]*)", "([^"]*)")/).map do |s|
+          DatabaseSchemaOwnership::Entity.new(s[1], s[0])
         end
       end
     end
--- rails8-example/db/database_schema_ownership/users.rb
+++ rails8-example/db/database_schema_ownership/users.rb
@@ -3,4 +3,3 @@
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  add_foreign_key "locations", "users"
